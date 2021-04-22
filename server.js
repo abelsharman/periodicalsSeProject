ce6abcaa7448b8af367c1ef88fa9677ef2dfc5cb
Tlek Suyerbassov <tleksuerbasov@mail.ru>
2021-04-22 09:48:48 +0600

    new:setup nginx

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+
+const path = __dirname + '/app/views/';
+app.use(express.static(path));
+
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {    
+    res.sendFile(path + "index.html");
+});
+
+app.get("/main", (req, res) => {    
+  res.sendFile(path + "index.html");
+});
+
+
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
